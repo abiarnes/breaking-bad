95951dce0b913ae8d9ace18c5ea08aabe3a26c59
Adriana Biarnés García <adrianabg@MacBook-de-Adriana.local>
2022-09-19 19:39:39 +0200

    added jest and react testing library config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Container } from '@mui/material';
 import Header from './components/Header';
 import List from './components/List';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import logo from '../media/logo1.png';
 
 const Header = () => {
     return (
-        <header>
+        <header data-testid="header">
             <img
                 src={logo}
                 alt=''
                 height={300}
                 className='logo'
-                
             />
         </header>
     );
